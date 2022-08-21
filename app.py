90d8c9ae0c2cb1e8435793e7873388b60b7e9a87
Hussain <mrrobogen@gmail.com>
2022-08-21 22:10:52 +0500

    Fixed tailwind install, added some routes

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+from asyncore import file_wrapper
+from crypt import methods
 from flask import Flask, render_template, session, request, redirect
 from flask_mail import Mail
+from pytube import YouTube
+import moviepy.editor as mp
 
 # Set flask app
 app = Flask("__name__")
 
-# Configure secret key for sessions
-app.secret_key = '06a8237a129aac9be5c85528626dfe946cfe6a3079035cd46b2c8bfbcf9e9ee0'
+# Makes sure templates auto reload
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # The following app with test to download youtube videos
-@app.route("/")
+
+# Dont rout to '/' for post request
+@app.route("/video-download")
 def index():
     return render_template("index.html")
+
+def get_video(url):
+    video_obj = YouTube(url)
+    video_obj.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download('./Downloads')
+
+
