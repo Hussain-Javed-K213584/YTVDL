2f45f23ded3ab0856c30650d700c4a659d98d378
Hussain <mrrobogen@gmail.com>
2022-08-21 14:21:02 +0500

    Created requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect
 from flask_mail import Mail
 
-secret_key = Flask.secret_key()
-print(secret_key)
+# Set flask app
+app = Flask("__name__")
+
+# Configure secret key for sessions
+app.secret_key = '06a8237a129aac9be5c85528626dfe946cfe6a3079035cd46b2c8bfbcf9e9ee0'
+
+# The following app with test to download youtube videos
+@app.route("/")
+def index():
+    return render_template("index.html")
