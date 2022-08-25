21f9b74d0e359c39e7d0b463daa2bfa5dcbefdea
Hussain <mrrobogen@gmail.com>
2022-08-25 12:01:25 +0500

    Created the functionality for user to download videos in different quality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, session, request, redirect
+from flask import Flask, render_template, session, request, redirect, send_file
 from pytube import YouTube
-import moviepy.editor as mp
+from io import BytesIO
 
 # Set flask app
 app = Flask("__name__")
@@ -10,7 +10,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # The following app with test to download youtube videos
 
-# URL: https://www.youtube.com/watch?v=Mz97sjRMSzg&ab_channel=Radixerus
+# URL: https://www.youtube.com/watch?v=ndsaoMFz9J4&ab_channel=Markiplier
 
 # Dont route to '/' for post request
 @app.route("/")
@@ -21,6 +21,30 @@ def index():
 def result():
     link = request.form.get("link")
     yt = YouTube(link)
-    return render_template("result.html", title=yt.title, author=yt.author, image=yt.thumbnail_url)
+    return render_template("result.html", title=yt.title, author=yt.author, image=yt.thumbnail_url, link=link)
+
+@app.route("/download", methods=['GET', 'POST'])
+def get_video():
+    if request.method == "POST":
+        link = request.form.get("download-button")
+        quality = request.form.get("quality")
+        print(quality)
+        yt = YouTube(link)
+        print(link)
+        if quality[-1] == "p":
+            yt = yt.streams.filter(progressive=True, file_extension='mp4')
+            yt = yt.get_by_resolution(quality)
+            buffer = BytesIO() # Use buffer to not to download file on server
+            yt.stream_to_buffer(buffer)
+            buffer.seek(0)
+            return send_file(buffer, as_attachment=True, download_name=yt.title + ".mp4", mimetype="video/mp4")
+        elif quality == "mp3":
+            yt = yt.streams.filter(progressive=True, file_extension='mp4')
+            yt = yt.get_highest_resolution()
+            buffer = BytesIO() # Use buffer to not to download file on server
+            yt.stream_to_buffer(buffer)
+            buffer.seek(0)
+            return send_file(buffer, as_attachment=True, download_name=yt.title + ".mp3", mimetype="audio/mp3")
+    return redirect("/")
 
 
